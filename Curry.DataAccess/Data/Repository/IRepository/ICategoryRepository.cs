1561da8cf2cccb06a552da5a7fef0fc658c292ae
qa <qa@readinghorizons.com>
2020-01-18 10:08:25 -0700

    IRepositoryCategory and RepositoryCategory Set Up

--- Curry.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
+++ Curry.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Curry.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Curry.DataAccess.Data.Repository.IRepository
 {
-    interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListOrDropdown();
+
+        void Update(Category category);
     }
 }
