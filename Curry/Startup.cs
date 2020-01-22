95cf698a23b585653a717f5d411e55a837c3173a
qa <qa@readinghorizons.com>
2020-01-21 19:35:41 -0700

    Category Controller built

--- Curry/Startup.cs
+++ Curry/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Curry.DataAccess.Data;
 using Curry.DataAccess;
+using Curry.DataAccess.Data.Repository.IRepository;
+using Curry.DataAccess.Data.Repository;
 
 namespace Curry
 {
@@ -28,6 +30,7 @@ namespace Curry
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
