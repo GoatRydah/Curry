b628be3c020a29e494745a56512f21a58743faef
qa <qa@readinghorizons.com>
2020-01-23 14:52:29 -0700

    Fixed some issues.

--- Curry.DataAccess/Data/Repository/UnitOfWork.cs
+++ Curry.DataAccess/Data/Repository/UnitOfWork.cs
@@ -8,18 +8,20 @@ namespace Curry.DataAccess.Data.Repository
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
 
         //Grabs a connection to the actual db to connect to this class
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
+            Category = new CategoryRepository(_db);
         }
 
-        public ICategoryRepository Category { get; private set; }
+
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void Save()
