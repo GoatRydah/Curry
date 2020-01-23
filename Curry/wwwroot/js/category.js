9d7a1d6996bce08c27cf10065c041bfc142cd6ea
qa <qa@readinghorizons.com>
2020-01-22 22:09:22 -0700

    Working on category.js

--- Curry/wwwroot/js/category.js
+++ Curry/wwwroot/js/category.js
@@ -1,1 +1,36 @@
-﻿
+﻿var dataTable;
+
+$(document).ready(function () {
+    LoadList();
+});
+
+function LoadList() {
+    dataTable = $('#DT_Load').DataTable({
+        "ajax": {
+            "url": "/api/category/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "40%" },
+            { "data": "displayOrder", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ` <div class="text-center">
+                                <a href="/Admin/category/upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                    <i class="far fa-edit"></i> Edit
+                                </a>
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onClick=Delete('/api/category/'+${data})>
+                                    <i class="far fa-trash-alt"></i> Delete
+                                </a>
+                             </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
